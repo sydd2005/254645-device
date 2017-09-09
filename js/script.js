300657b98358e8f787ca0115aa33ac59ee840135
Spiridonov Dmitry <sydd2005@gmail.com>
2017-09-09 18:54:13 +0300

    ready for review

---
+++ js/script.js
@@ -0,0 +1,27 @@
+var link = document.querySelector(".write-us");
+var popup = document.querySelector(".write-us-overlay");
+var closeButton = document.querySelector(".modal-write-us .modal-close");
+
+link.addEventListener("click", function (event) {
+    event.preventDefault();
+    popup.classList.add("modal-show");
+});
+
+closeButton.addEventListener("click", function (event) {
+    event.preventDefault();
+    popup.classList.remove("modal-show");
+});
+
+var mapLink = document.querySelector(".map-link");
+var mapPopup = document.querySelector(".map-overlay");
+var closeButton = document.querySelector(".modal-map .modal-close");
+
+mapLink.addEventListener("click", function (event) {
+    event.preventDefault();
+    mapPopup.classList.add("modal-show");
+});
+
+closeButton.addEventListener("click", function (event) {
+    event.preventDefault();
+    mapPopup.classList.remove("modal-show");
+});
