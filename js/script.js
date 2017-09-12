7989e24bedfbcceb5008e02b36b41d9f049406d8
Spiridonov Dmitry <sydd2005@gmail.com>
2017-09-12 23:35:27 +0300

    added form animation on validation error

--- js/script.js
+++ js/script.js
@@ -1,15 +1,28 @@
 var link = document.querySelector(".write-us");
 var popup = document.querySelector(".write-us-overlay");
-var closeButton = document.querySelector(".modal-write-us .modal-close");
+var form = popup.querySelector("form");
+var fullname = form.querySelector("[name=fullname]");
+var closeButton = popup.querySelector(".modal-close");
 
 link.addEventListener("click", function (event) {
 	event.preventDefault();
 	popup.classList.add("modal-show", "animated-modal-show");
 });
 
+form.addEventListener("submit", function(event) {
+	if (!fullname.value) {
+		event.preventDefault();
+		fullname.classList.add("invalid");
+		popup.classList.remove("modal-error");
+		popup.offsetWidth = popup.offsetWidth;
+		popup.classList.add("modal-error");
+	}
+});
+
 closeButton.addEventListener("click", function (event) {
 	event.preventDefault();
-	popup.classList.remove("modal-show", "animated-modal-show");
+	fullname.classList.remove("invalid");
+	popup.classList.remove("modal-show", "animated-modal-show", "modal-error");
 });
 
 var mapLink = document.querySelector(".map-link");
@@ -24,4 +37,16 @@ mapLink.addEventListener("click", function (event) {
 mapCloseButton.addEventListener("click", function (event) {
 	event.preventDefault();
 	mapPopup.classList.remove("modal-show");
+});
+
+window.addEventListener("keydown", function(event) {
+	if (event.keyCode === 27) {
+		if(popup.classList.contains("modal-show")) {
+			fullname.classList.remove("invalid");
+			popup.classList.remove("modal-show", "animated-modal-show", "modal-error");
+		}
+		if(mapPopup.classList.contains("modal-show")) {
+			mapPopup.classList.remove("modal-show");
+		}
+	}
 });
