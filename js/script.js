48a4d72c615a2b9f40444335830715b811bb4d56
Spiridonov Dmitry <spiridonov@fgosniias.ru>
2017-09-12 16:38:13 +0300

    fixes after first attempt

--- js/script.js
+++ js/script.js
@@ -3,25 +3,25 @@ var popup = document.querySelector(".write-us-overlay");
 var closeButton = document.querySelector(".modal-write-us .modal-close");
 
 link.addEventListener("click", function (event) {
-    event.preventDefault();
-    popup.classList.add("modal-show");
+	event.preventDefault();
+	popup.classList.add("modal-show", "animated-modal-show");
 });
 
 closeButton.addEventListener("click", function (event) {
-    event.preventDefault();
-    popup.classList.remove("modal-show");
+	event.preventDefault();
+	popup.classList.remove("modal-show", "animated-modal-show");
 });
 
 var mapLink = document.querySelector(".map-link");
 var mapPopup = document.querySelector(".map-overlay");
-var closeButton = document.querySelector(".modal-map .modal-close");
+var mapCloseButton = document.querySelector(".modal-map .modal-close");
 
 mapLink.addEventListener("click", function (event) {
-    event.preventDefault();
-    mapPopup.classList.add("modal-show");
+	event.preventDefault();
+	mapPopup.classList.add("modal-show");
 });
 
-closeButton.addEventListener("click", function (event) {
-    event.preventDefault();
-    mapPopup.classList.remove("modal-show");
+mapCloseButton.addEventListener("click", function (event) {
+	event.preventDefault();
+	mapPopup.classList.remove("modal-show");
 });
